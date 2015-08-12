fb341437a9ee5a3b685990ec4333d79926326c9c
jerry08576 <104021033@live.asia.edu.tw>
2015-08-12 17:00:49 +0800

    作業6
    陣列Array2

--- JavaDay7/src/D7java4.java
+++ JavaDay7/src/D7java4.java
@@ -6,13 +6,30 @@ public class D7java4 {
 		// TODO Auto-generated method stub
 		Random rnd = new Random();
 		Scanner scn = new Scanner(System.in);
-		System.out.println("請輸入五個不重複數字!");
-		int m=scn.nextInt();
 		int[]data = new int[5];
-		int i=0,j=0;
+		int[]data1 = new int[5];
+		int i=0,j=0,c=0;
+		System.out.println("請輸入五個不重複數字!");
+		for(c=0;c<5;c++){
+			data1[c]=scn.nextInt();
+	}
+		for(c=0;c<5;c++){
+			for(j=c+1;j<5;j++){
+				if(data1[c]>data1[j]){
+					int v1=data1[c];
+					data1[c]=data1[j];
+					data1[j]=v1;
+				}
+			}
+		}
+     	
+		
+		
+		System.out.println("中位數為"+data1[c=2]);
+	
 		boolean flag=false;
 		while(i<5){
-			data[i]=rnd.nextInt(100);
+			data[i]=rnd.nextInt(100)+1;
 			flag=false;
 			for( j=0;j<i;j++){
 				if(data[i] == data[j]){
@@ -23,6 +40,7 @@ public class D7java4 {
 				i++;
 			}
 		}
+		
 		for(i=0;i<5;i++){
 			for(j=i+1;j<5;j++){
 				if(data[i]>data[j]){
@@ -31,9 +49,20 @@ public class D7java4 {
 					data[j]=v1;
 				}
 			}
-		}
-		for(i=0;i<5;i++){
 			System.out.println(data[i]);
-			}
+
+		}
+		
+		System.out.println("中位數為"+data[i=2]);
+		if(data1[c]>data[i]){
+			System.out.println("贏了!");
+		}else if(data1[c]<data[i]){
+			System.out.println("輸了!");
+		}
+		else{
+			System.out.println("平手!!");
+		}
+
+
 	}
 }
